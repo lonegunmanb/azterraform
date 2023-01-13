c0264c76444ea3bab37770c0e6d347381de6e180
zjhe <hezijie@microsoft.com>
2023-01-13 18:24:20 +0800

    change rg name

--- quickstart/101-resource-group/main.tf
+++ quickstart/101-resource-group/main.tf
@@ -4,5 +4,5 @@ resource "random_pet" "rg_name" {
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  name     = "rg-${random_pet.rg_name.id}"
 }
