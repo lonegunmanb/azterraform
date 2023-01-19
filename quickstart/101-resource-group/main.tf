51ae00be52910c2bc9bd9ac09bf5af522c3e1fc8
zjhe <hezijie@microsoft.com>
2023-01-20 06:30:03 +0800

    upgrade rg name

--- quickstart/101-resource-group/main.tf
+++ quickstart/101-resource-group/main.tf
@@ -4,5 +4,5 @@ resource "random_pet" "rg_name" {
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  name     = "${random_pet.rg_name.id}-rg"
 }
