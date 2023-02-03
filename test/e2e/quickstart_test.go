70a74e0e11d428abc44c8182774f398120570672
hezijie <lonegunmanb@hotmail.com>
2023-02-03 10:05:14 +0800

    upgrade ci

--- test/e2e/quickstart_test.go
+++ test/e2e/quickstart_test.go
@@ -31,43 +31,10 @@ func Test_Quickstarts(t *testing.T) {
 			continue
 		}
 		t.Run(f, func(t *testing.T) {
-			defer func() {
-				recordTestVersionSnapshot(t, f)
-			}()
 			helper.RunE2ETest(t, path, "", terraform.Options{
 				Upgrade: true,
 			}, nil)
 		})
-	}
-}
-
-func recordTestVersionSnapshot(t *testing.T, f string) {
-	if t.Failed() {
-		return
-	}
-	err := helper.RecordVersionSnapshot(t, filepath.Join("..", ".."), f)
-	if err != nil {
-		t.Fatalf("Error when generate test version snapshot for %s: %s", f, err.Error())
-	}
-	_, f = filepath.Split(f)
-	dstFolder := filepath.Clean(filepath.Join("..", "..", "TestRecord", f))
-	if !files.IsExistingDir(dstFolder) {
-		err = os.MkdirAll(dstFolder, os.ModeDir)
-		if err != nil {
-			t.Fatalf("Error when make dir for test version snapshot for %s: %s", f, err.Error())
-		}
-	}
-
-	dst := filepath.Clean(filepath.Join(dstFolder, "TestRecord.md.tmp"))
-	if files.FileExists(dst) {
-		err := os.Remove(dst)
-		if err != nil {
-			t.Fatalf("Error when delete existing test version snapshot %s: %s", dst, err.Error())
-		}
-	}
-	err = files.CopyFile(filepath.Clean(filepath.Join("..", "..", "quickstart", f, "TestRecord.md.tmp")), dst)
-	if err != nil {
-		t.Fatalf("Error when copy test version snapshot for %s: %s", f, err.Error())
 	}
 }
 
